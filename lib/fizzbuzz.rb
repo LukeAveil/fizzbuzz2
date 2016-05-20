5108a7bbaac5aacdbcef85613dbe2e4f9118bb7b
Luke <lukeaveil@yahoo.com>
2016-05-20 12:47:39 +0100

    change method to meet new test

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,10 +1,14 @@
-def fizzbuzz(number)
-	if number % 15 == 0
+class Fixnum
+
+def fizzbuzz
+	if self % 15 == 0
 		"fizzbuzz"
-	elsif number % 3 == 0
+	elsif self % 3 == 0
 		"fizz"
-	elsif number % 5 == 0
+	elsif self % 5 == 0
 		"buzz"
-	else number
+	else self
 	end
+end
+
 end
